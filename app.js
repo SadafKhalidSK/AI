f42cd37cf92491669d7195be4586279043cd6f14
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-02-24 05:45:23 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var message = "HELLO WORLD";
+console.log(message);
+var message1 = "3rd Class";
+console.log(message1);
+//var can be redeclare....let cannot be redeclare
+var color = "blue";
+var color = "red";
+console.log(color);
+//declaration and initializtion
+var colour;
+colour = "yellow";
+console.log(colour);
+//cont
+// Cannot redeclare block-scoped variable 'message'.
+var mes = "sad";
+console.log(mes);
+//syntax is the rule to write program its predefine
+// CASE
+//camelCaseHere
+//var cannot be started by any number,@,%,~,++,# 
+var userName = "Seniya";
+var Age = 100;
+var ayesha_Alive = true;
+console.log(ayesha_Alive);
